94f30cfbcd09b1ed225e60d0ac7804fa538c5743
ThankGod Egbo <thankgodegbo@gmail.com>
2021-03-18 08:09:02 +0100

    Responsiveness Fix

--- src/Steps/stepFive.js
+++ src/Steps/stepFive.js
@@ -1,10 +1,10 @@
 import Logo from "assets/img/logo.png";
 import { Illustration, FormContainer } from "styles/style"
 import IllustrationImage from "assets/img/address-info.png"
-import Input from "components/Input"
 import Form from "styles/FormBlock";
+import Autocomplete from 'react-google-autocomplete';
 
-const StepFive = (props) => {
+const StepFive = ({ formData, setFormData }) => {
     return (
         <>
             <Illustration>
@@ -15,15 +15,47 @@ const StepFive = (props) => {
                 <h1>WHAT ADDRESS ARE YOU MOVING FROM?</h1>
                 <h4 style={{ width: "426px" }}>We’ll send our highly trained crew to this address to perform the services you have selected.</h4>
                 <Form>
-                    <Input
-                        label={"Enter Address"}
-                        name={"address"}
-                        id={"address"}
-                        placeholder="e.g 78, Bode Thomas Street"
-                        onChange={(e) => {
-                            props.formData.address = e.target.value
-                            props.setFormData({ ...props.formData })
+                    <p
+                        style={{
+                            fontFamily: "Montserrat",
+                            display: "block",
+                            color: "#8C98A9",
+                            fontWeight: 400,
+                            fontSize: "12px",
+                            marginBottom: "10px",
+                            width: "93%"
                         }}
+                    >
+                        Enter Address
+                    </p>
+                    <Autocomplete
+                        name="address"
+                        id="address"
+                        placeholder="e.g 78, Bode Thomas Street"
+                        style={{
+                            background: "#E0E7FF33",
+                            boxShadow: "0px 3px 6px #00000005",
+                            borderRadius: "5px",
+                            opacity: 1,
+                            fontSize: "13px",
+                            boxSizing: "border-box",
+                            border: "1px solid #E0E7FF",
+                            padding: "15px 14px",
+                            width: "100%",
+                            maxWidth: "486px",
+                            height: "40px",
+                        }}
+                        className="autocomplete"
+                        onBlur={(e) => {
+                            formData.address = e.target.value
+                            setFormData({ ...formData })
+                        }}
+                        onPlaceSelected={(place) => {
+                            formData.address = place.formatted_address
+                            setFormData({ ...formData })
+                        }}
+                        types={["geocode"]}
+                        componentRestrictions={{ country: "ng" }}
                     />
                 </Form>
             </FormContainer>
